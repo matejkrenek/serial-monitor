46d16bcb6214c5b6cf190ed9d5e0a68ad32fc0ec
matej.krenek <krenek@izon.cz>
2023-03-30 12:34:20 +0200

    add packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,30 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Alert, Button } from './components'
-
+import Select from 'react-select'
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+]
 const App: React.FC = () => {
-    console.log(useState())
-    const [isVisible, setIsVisible] = useState(true)
+    const [state, setState] = React.useState(true)
 
     return (
         <div className="bg-gray-100 h-full">
-            {isVisible && (
-                <header className="bg-teal-400 px-4 py-2">
-                    <div className="flex items-center justify-between">
-                        <div className="-mx-1">
-                            <Button className="mx-1">Connect</Button>
-                            <Button className="mx-1" color="secondary">
-                                Disconnect
-                            </Button>
-                        </div>
+            <header className="bg-teal-400 px-4 py-2">
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center -mx-1">
+                        <Select isSearchable={false} options={options} className="h-8" />
+                        <Button className="mx-1">Connect</Button>
+                        <Button className="mx-1" color="secondary">
+                            Disconnect
+                        </Button>
                     </div>
-                </header>
-            )}
+                </div>
+            </header>
+
             <Alert
                 type="error"
-                text="Sorry, something went wrong please try again."
-                className="my-4"
-            />
-            <Alert
-                type="info"
-                text="Sorry, something went wrong please try again."
-                className="my-4"
-            />
-            <Alert
-                type="success"
-                text="Sorry, something went wrong please try again."
-                className="my-4"
-            />
-            <Alert
-                type="warning"
                 text="Sorry, something went wrong please try again."
                 className="my-4"
             />
